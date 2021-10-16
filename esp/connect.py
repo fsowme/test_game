89e6a73e0cab8b8f82a17cbf2c8be8fd97b8fc71
code <fsow@yandex.ru>
2021-10-16 21:15:09 +0300

    fix mock ESPManager

--- esp/connect.py
+++ esp/connect.py
@@ -6,7 +6,7 @@ from esp.abs import BaseESPManager
 
 class ESPManager(BaseESPManager):
     def user_exists(self, email) -> bool:
-        return random.choice([False])
+        return random.choice([True, False])
 
     def add_user(self, email):
         pass
